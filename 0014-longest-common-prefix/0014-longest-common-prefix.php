4edd350fdb9fb93459098d11567d9c77ead55d18
Amine Mrani <122173068+mranidev@users.noreply.github.com>
2023-09-16 07:36:56 -0400

    Time: 9 ms (46.92%), Space: 18.9 MB (93.70%) - LeetHub

---
+++ 0014-longest-common-prefix/0014-longest-common-prefix.php
@@ -0,0 +1,28 @@
+class Solution {
+
+    /**
+     * @param String[] $strs
+     * @return String
+     */
+    function longestCommonPrefix($strs) {
+        
+        $prefix = '';
+
+        for ($i=0; $i<strlen($strs[0]); $i++)
+        {
+            $prefix .= $strs[0][$i];
+            foreach($strs as $str)
+            {
+                if (substr($str, 0, strlen($prefix)) == $prefix)
+                {
+                    continue;
+                }
+                else 
+                {
+                    return substr($prefix, 0, -1);
+                }
+            }
+        }
+        return $prefix;
+    }
+}
